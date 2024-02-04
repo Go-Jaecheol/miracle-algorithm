24c1b53eb4252e28bf2c7012d91bf89169a5f8d8
JFe <33208246+Go-Jaecheol@users.noreply.github.com>
2024-02-04 23:45:01 +0900

    [boj] 최솟값과 최댓값 (Time: 1548ms, Memory: 155.3MB) - by Code-Vault

---
+++ boj/[2357] 최솟값과 최댓값/python/2357.py
@@ -0,0 +1,39 @@
+import sys
+from math import ceil, log
+
+
+def segment(idx, start, end):
+    if start == end:
+        tree[idx] = (nums[start], nums[start])
+        return tree[idx]
+
+    mid = (start + end) // 2
+    left_child = segment(idx * 2, start, mid)
+    right_child = segment(idx * 2 + 1, mid + 1, end)
+
+    tree[idx] = (min(left_child[0], right_child[0]), max(left_child[1], right_child[1]))
+    return tree[idx]
+
+
+def search(idx, start, end, search_range1, search_range2):
+    if search_range1 > end or search_range2 < start:
+        return float('inf'), 0
+    if search_range1 <= start and end <= search_range2:
+        return tree[idx]
+    mid = (start + end) // 2
+    left_child = search(idx * 2, start, mid, search_range1, search_range2)
+    right_child = search(idx * 2 + 1, mid + 1, end, search_range1, search_range2)
+    return min(left_child[0], right_child[0]), max(left_child[1], right_child[1])
+
+
+N, M = map(int, sys.stdin.readline().split())
+nums = [int(sys.stdin.readline().strip()) for _ in range(N)]
+pairs = [[int(x) for x in sys.stdin.readline().split()] for _ in range(M)]
+
+h = ceil(log(N, 2) + 1)
+tree = [0 for _ in range(pow(2, h))]
+segment(1, 0, N - 1)
+
+for a, b in pairs:
+    answer = search(1, 0, N - 1, a - 1, b - 1)
+    print(answer[0], answer[1])
---
+++ boj/[2357] 최솟값과 최댓값/python/README.md
@@ -0,0 +1,8 @@
+# [2357] 최솟값과 최댓값
+
+[문제 출처](https://www.acmicpc.net/problem/2357)
+
+## 성능
+
+- Time: 1548ms
+- Memory: 155.3MB
